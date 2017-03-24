86d4a799b1c0867f015f192f212a1e3b4d692076
Wilfrido T. Nuqui Jr <dofreewill22@gmail.com>
2017-03-24 16:54:20 +0800

    Reorganize PromisepayEx.address test and fixture

--- test/promisepay_ex/addresses/address_test.exs
+++ test/promisepay_ex/addresses/address_test.exs
@@ -1,9 +1,12 @@
-defmodule AddressesTest do
+defmodule GetAddressTest do
   use PromisepayEx.ApiCase
 
-  test "address request" do
-    use_cassette "address_request" do
+  test "address returns Map (200)" do
+    use_cassette "addresses/get/200" do
       address = PromisepayEx.address("1fa89e62-0abe-432c-81f0-10e9f7f15f74")
+
+      assert is_map(address)
+
       assert address.id == "1fa89e62-0abe-432c-81f0-10e9f7f15f74"
       assert address.addressline1 == "6880 Purdy Forges"
       assert address.country == "United States of America"
