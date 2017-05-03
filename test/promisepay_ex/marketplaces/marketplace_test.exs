f26435cad36291f3e9139982351630a0f5160479
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-03 10:39:16 +0800

    Reduce chars in line to make code climate happy

--- test/promisepay_ex/marketplaces/marketplace_test.exs
+++ test/promisepay_ex/marketplaces/marketplace_test.exs
@@ -3,7 +3,8 @@ defmodule GetMarketplaceTest do
 
   test "marketplace returns Map (200)" do
     use_cassette "marketplaces/get/200" do
-      marketplace = PromisepayEx.marketplace("7871c10a-a457-4cc6-91f5-a49c147d7c9f")
+      marketplace_id = "7871c10a-a457-4cc6-91f5-a49c147d7c9f"
+      marketplace = PromisepayEx.marketplace(marketplace_id)
 
       assert is_map(marketplace)
 
