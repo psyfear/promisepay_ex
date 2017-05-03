f55854032f098f3099806da6f3d66c8e3bb86f2f
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-03 10:15:32 +0800

    Add tests and vcr fixtures

---
+++ test/promisepay_ex/marketplaces/marketplace_test.exs
@@ -0,0 +1,14 @@
+defmodule GetMarketplaceTest do
+  use PromisepayEx.ApiCase
+
+  test "marketplace returns Map (200)" do
+    use_cassette "marketplaces/get/200" do
+      marketplace = PromisepayEx.marketplace("7871c10a-a457-4cc6-91f5-a49c147d7c9f")
+
+      assert is_map(marketplace)
+
+      assert marketplace.name == "Awesome Websites"
+      assert marketplace.short_name == "awesome"
+    end
+  end
+end
