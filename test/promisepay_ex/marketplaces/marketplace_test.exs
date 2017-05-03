51b0b53eada84de80ba1f5c71883bc3d12bfb5c1
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-03 10:41:38 +0800

    Fix indentation to satisfy char limit

--- test/promisepay_ex/marketplaces/marketplace_test.exs
+++ test/promisepay_ex/marketplaces/marketplace_test.exs
@@ -3,8 +3,9 @@ defmodule GetMarketplaceTest do
 
   test "marketplace returns Map (200)" do
     use_cassette "marketplaces/get/200" do
-      marketplace_id = "7871c10a-a457-4cc6-91f5-a49c147d7c9f"
-      marketplace = PromisepayEx.marketplace(marketplace_id)
+      marketplace = PromisepayEx.marketplace(
+        "7871c10a-a457-4cc6-91f5-a49c147d7c9f"
+      )
 
       assert is_map(marketplace)
 
